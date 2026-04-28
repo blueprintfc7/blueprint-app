4aba667372762a207da7e5280af3b24f05ab9311
blueprintfc7 <blueprintfc7@gmail.com>
2026-04-28 12:43:40 +1000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import '@/index.css'
+import './index.css'
 
 // Auto-apply dark class based on system preference (only if user hasn't manually overridden)
 if (!document.documentElement.classList.contains('light') && !document.documentElement.classList.contains('dark')) {
